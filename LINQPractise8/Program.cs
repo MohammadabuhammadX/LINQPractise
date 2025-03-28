49d902f15c631d32876628e0d0b7445deddfa761
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-03-28 14:58:52 +0300

    q.9

---
+++ LINQPractise8/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LINQPractise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*9. Write a program in C# Sharp to create a list of numbers and display numbers greater than 80.
+                Test Data :
+                The members of the list are :
+                55 ,200 ,740 ,76 ,230 ,482 ,95
+                Expected Output :
+                The numbers greater than 80 are :
+                200
+                740
+                230
+                482
+                95
+            */
+
+            var testDataList = new List<int> { 55, 200, 740, 76, 230, 482, 95 };
+
+
+            var query = testDataList.FindAll(x => x > 80 ? true : false);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
