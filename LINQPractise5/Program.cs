70ca1f900844d663d41c5846e131acf1de31a9d0
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-03-28 04:58:18 +0300

    q.6

---
+++ LINQPractise5/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LINQPractise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*6. Write a program in C# Sharp to display the name of the days of the week.
+                Expected Output:
+                Sunday
+                Monday
+                Tuesday
+                Wednesday
+                Thursday
+                Friday
+                Saturday
+            */
+
+
+            //string[] dayWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            //var days = dayWeek.Select(i => i);
+
+            //foreach (var day in days)
+            //{
+            //    Console.WriteLine(day);
+            //}
+
+            var daysOfWeek = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>().Select(day=>day).ToArray();
+
+            foreach (var day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+        }
+    }
+}
