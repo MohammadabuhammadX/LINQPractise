2ad9e4b7d11007dfed84a194f6a1dfbc5dde7668
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-03-28 04:34:21 +0300

    Add LINQPractise2 project and update solution settings
    Updated VisualStudioVersion in LINQPractise.sln to 17.12.35707.178 d17.12.
    Added new project LINQPractise2 with .NET 8.0 target framework.
    Configured LINQPractise2.csproj with Microsoft.NET.Sdk, Exe output type,
    and enabled implicit usings and nullable reference types.
    Created Program.cs in LINQPractise2 with a LINQ query to square numbers.

---
+++ LINQPractise2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LINQPractise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program in C# Sharp to find the number of an array and the square of each number.
+                Expected Output :
+                { Number = 9, SqrNo = 81 }
+                { Number = 8, SqrNo = 64 }
+                { Number = 6, SqrNo = 36 }
+                { Number = 5, SqrNo = 25 }
+            */
+
+            var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var query = arr1.Select(x => new 
+            {
+                num =x,
+                Sqr = x*x
+            });
+
+            foreach (var x in query)
+            {
+                Console.WriteLine($"Number : {x.num} , Square : {x.Sqr}");
+            }
+        }
+    }
+}
