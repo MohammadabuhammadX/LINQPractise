cd555a450be133e5b4958bf362629f02ef356889
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-03-28 04:17:10 +0300

    Add project files.

---
+++ LINQPractise/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LINQPractise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>() 
+            {
+                0,2,4,6,8
+            };
+
+            var evenNumbers = num.Where(n => n % 2 == 0);
+
+            Console.WriteLine("Even Numbers");
+            foreach (int n in evenNumbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
