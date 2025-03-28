22cc1e256c53c05bafc581b8d9227117ad734f87
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-03-28 04:50:41 +0300

    Q.5

--- LINQPractise3/Program.cs
+++ LINQPractise3/Program.cs
@@ -12,14 +12,16 @@
             Number 1 appears 1 times
             */
 
-            int[] arr1 = { 5, 5, 5, 9, 9, 1 ,234,1231,123,2,234,2342,42,342,4,234,1,3,12,312,31,231,23,123,12,31,23,12,312,};
+            int[] arr1 = { 5, 5, 9, 9, 1 ,234,1231,123,2,234,2342,42,342,4,234,1,3,12,312,31,231,23,123,12,12,12,12,12,31,23,12,312,};
 
             var query = arr1.GroupBy(x => x)
                             .Select(x => new 
                             {
                                  Number = x.Key,
                                  Frequencies =x.Count()
-                            }).Take(3);
+                            })
+                            .OrderByDescending(x => x.Frequencies)
+                            .Take(3);
 
             foreach ( var item in query )
             {
---
+++ LINQPractise4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LINQPractise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*5. Write a program in C# Sharp to display the characters and frequency of each character in a given string.
+                Test Data:
+                Input the string: apple
+                Expected Output:
+                The frequency of the characters are :
+                Character a: 1 times
+                Character p: 2 times
+                Character l: 1 times
+                Character e: 1 times
+            */
+
+
+            string input =Console.ReadLine();
+
+            var query = input.GroupBy(x => x)
+                             .Select(x => new
+                             {
+                                 chr = x.Key,
+                                 freq = x.Count()
+                             });
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
