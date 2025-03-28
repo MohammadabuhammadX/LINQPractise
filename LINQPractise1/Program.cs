32954228795294a89727410a47603fde55bc1d93
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-03-28 04:23:04 +0300

    Add LINQPractise1 project with LINQ example
    A new project named `LINQPractise1` has been added to the Visual Studio solution file `LINQPractise.sln`. This includes the project configuration for both Debug and Release modes.
    
    A new project file `LINQPractise1.csproj` has been created for the `LINQPractise1` project. This project targets .NET 8.0 and has implicit usings and nullable reference types enabled.
    
    A new `Program.cs` file has been added to the `LINQPractise1` project. This file contains a C# program that uses LINQ to find positive numbers from a list of integers that are within the range of 1 to 11. The program outputs these numbers to the console.

---
+++ LINQPractise1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LINQPractise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2. Write a program in C# Sharp to find the +ve numbers from a list of numbers using two where conditions in LINQ Query.
+                Expected Output:
+                The numbers within the range of 1 to 11 are :
+                1 3 6 9 10
+            */
+
+            int[] n1 = {1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14};
+
+            var query = from num in n1
+                        where num > 0
+                        where num < 12
+                        select num;
+
+            foreach( var n in query )
+            {
+                Console.WriteLine( n);
+            }
+
+        }
+    }
+}
